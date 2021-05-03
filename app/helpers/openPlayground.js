1f9c493abbd4c1a206ba2e37715006d65a4e1cb9
Balthazar Gronon <git@balthazar.dev>
2021-05-03 14:49:39 -0700

    Support variables and headers in playground links

--- app/helpers/openPlayground.js
+++ app/helpers/openPlayground.js
@@ -3,6 +3,7 @@ const Handlebars = require('handlebars')
 const common = require('../lib/common')
 
 module.exports = (value, options) => {
+  const baseUrl = options.data.root.servers[0].url
   const query = value.example
   const variables = JSON.stringify(
     Object.keys(value.schema.properties).reduce((acc, key) => {
@@ -12,11 +13,18 @@ module.exports = (value, options) => {
     }, {}),
   )
 
-  const variablesQuery = variables ? `&variables=${encodeURIComponent(variables)}` : ''
+  const tabs = [
+    {
+      endpoint: baseUrl,
+      query,
+      variables,
+      headers: {
+        'X-APIKEY': 'Change this',
+      },
+    },
+  ]
 
-  const url = `${options.data.root.servers[0].url}?query=${encodeURIComponent(
-    query,
-  )}${variablesQuery}`
+  const url = `${baseUrl}?tabs=${encodeURIComponent(JSON.stringify(tabs))}`
 
   return new Handlebars.SafeString(`<a href="${url}" target="_blank">Open in playground<a/>\n`)
 }
